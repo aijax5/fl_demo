1c2c234e90991a2e96b85d9c40ca3cf162b3b4b2
ajaaz <mdajaaz1999@gmail.com>
2020-02-08 14:29:20 +0530

    copy from lappy

--- server.py
+++ server.py
@@ -32,6 +32,7 @@ class FedNetServer(object):
         self.clientCounter=0
         self.tempSum=0
         self.val={'num':0}
+        self.client_weights=[]
         #####
      
         # socket io messages
@@ -60,6 +61,19 @@ class FedNetServer(object):
 
     def deserializeObject(s):
         return pickle.loads(codecs.decode(s.encode(), "base64"))
+    def getAverages(self,client_weights):
+            new=[]
+            for i,weights in enumerate(client_weights):
+                if i == 0:
+                    new = weights
+                else:
+                    for j,weight in enumerate(weights):
+                        new[j] += weight
+            
+            for k,w in enumerate(new):
+                new[k] = np.divide(w,float(len(client_weights)))
+
+            return new
            
     def register_handles(self):
         # single-threaded async, no need to lock
@@ -89,19 +103,7 @@ class FedNetServer(object):
         def handle_client_ready(data):
             print("client ready ", self.clientCounter, data)
       
-        def getAverages(self,client_weights):
-            new=[]
-            for i,weights in enumerate(client_weights):
-                if i == 0:
-                    new = weights
-                else:
-                    for j,weight in enumerate(weights):
-                        new[j] += weight
-            
-            for k,w in enumerate(new):
-                new[k] = np.divide(w,float(len(client_weights)))
-
-            return new
+        
 
 
 
@@ -111,17 +113,18 @@ class FedNetServer(object):
             if self.getCount:
                 self.tempCount=self.clientCounter
                 self.getCount=False
-                client_weights=[]
+                
 
             self.tempCount -= 1
             data = FedNetServer.deserializeObject(data)
-            client_weights.append(data)
-            print("weights received ",len(client_weights))
+            self.client_weights.append(data)
+            print("weights received ",len(self.client_weights))
 
-            if tempCount == 0:
-                self.new_weights = self.getAverages(client_weights)
-                time.sleep(30)
+            if self.tempCount == 0:
+                self.new_weights = self.getAverages(self.client_weights)
+                time.sleep(3)
                 data=FedNetServer.serilalizeObject(self.new_weights)
+                self.getCount=True
                 emit('client_update',data,broadcast = True, namespace='/')
                 
             
